3f2854c85a727b1b10e3482761bc276f9ed71f7b
BAPAJO <111722830+BAPAJO@users.noreply.github.com>
2023-06-25 16:37:38 +0700

    Harga Bahan Per Gram dan Super class
    Harga Bahan Per Gram dan Super class harga jual

---
+++ UAS-PBO/src/main/java/Bahan/HargaPerGram.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bahan;
+
+/**
+ *
+ * @author bangu
+ */
+public class HargaPerGram {
+     public static double gramTepung(){
+        return HargaBahan.hargaTepung / BeratKemasan.kemasanTepung;
+    }
+    public static double gramGula(){
+        return HargaBahan.hargaGula / BeratKemasan.kemasanGula;
+    }
+    public static double gramButter(){
+        return HargaBahan.hargaButter / BeratKemasan.kemasanButter;
+    }
+    public static double gramRagi(){
+        return HargaBahan.hargaRagi / BeratKemasan.kemasanRagi;
+    }
+    public static double gramSusuBubuk(){
+        return HargaBahan.hargaSusuBubuk / BeratKemasan.kemasanSusuBubuk;
+    }
+    public static double gramSusuCair(){
+        return HargaBahan.hargaSusuCair / BeratKemasan.kemasanSusuCair;
+    }
+    public static double gramTelur(){
+        return HargaBahan.hargaTelur / BeratKemasan.kemasanTelur;
+    }
+    public static double gramEsBatu(){
+        return HargaBahan.hargaEsBatu / BeratKemasan.kemasanEsBatu;
+    }
+    public static double gramKeju(){
+        return HargaBahan.hargaKeju / BeratKemasan.kemasanKeju;
+    }
+    public static double gramCoklat(){
+        return HargaBahan.hargaCoklat / BeratKemasan.kemasanCoklat;
+    }
+    public static double gramKrim(){
+        return HargaBahan.hargaKrim / BeratKemasan.kemasanKrim;
+    }
+    public static double gramSelai(){
+        return HargaBahan.hargaSelai / BeratKemasan.kemasanSelai;
+    }
+    public static double gramSosis(){
+        return HargaBahan.hargaSosis / BeratKemasan.kemasanSosis;
+    }
+    public static double gramSmokedBeef(){
+        return HargaBahan.hargaSmokedBeef / BeratKemasan.kemasanSmokedBeef;
+    }
+    public static double gramBawang(){
+        return HargaBahan.hargaBawang / BeratKemasan.kemasanBawang;
+    }
+}
---
+++ UAS-PBO/src/main/java/com/mycompany/uas/pbo/UASPBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uas.pbo;
+
+/**
+ *
+ * @author bangu
+ */
+public class UASPBO {
+
+    public static void main(String[] args) {
+       
+    }
+}
