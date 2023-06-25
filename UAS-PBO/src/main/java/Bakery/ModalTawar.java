80b2bd0a363082ae7ff169481f9075e881016a19
Mawsali <111742299+Mawsali@users.noreply.github.com>
2023-06-25 17:14:07 +0700

    ModalTawar

--- UAS-PBO/src/main/java/Bakery/ModalTawar.java
+++ UAS-PBO/src/main/java/Bakery/ModalTawar.java
@@ -9,5 +9,8 @@ package Bakery;
  * @author acer
  */
 public class ModalTawar {
-    
+     public void modalAdonan(){
+        return (this.adonanTepungTerigu()*HargaPerGram.gramTepungTerigu + this.adonanGula()*hargaPerGram.gramGula + this.adonanButter()*hargaPerGram.gramButter + this.adonanRagi()*hargaPerGram.gramRagi + this.adonanSusuBubuk()*hargaPerGram.gramSusuBubuk + this.adonanSusuCair()*hargaPerGram.gramSusuCair + this.adonanTelur()*hargaPerGram.gramTelur + this.adonanEsBatu()*hargaPerGram.gramEsBatu);
+      
+    }
 }
