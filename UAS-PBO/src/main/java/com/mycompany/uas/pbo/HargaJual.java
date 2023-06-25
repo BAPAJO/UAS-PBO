388b5342073da4f567a9731ea5dc7e64b0d34f63
BAPAJO <111722830+BAPAJO@users.noreply.github.com>
2023-06-25 16:46:28 +0700

    Create HargaJual.java

---
+++ UAS-PBO/src/main/java/com/mycompany/uas/pbo/HargaJual.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.uas.pbo;
+
+/**
+ *
+ * @author bangu
+ */
+public class HargaJual {
+    // method
+    abstract double varian1();
+    abstract double varian2();
+    abstract double varian3();
+
+    // diisi dengan print(modal x 140%)
+    abstract String hargaJual();
+}
