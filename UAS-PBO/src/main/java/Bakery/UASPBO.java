e54b9a8b7931ee3cb4d487024f358fc0fb3e8dd3
safiirahsfh <111759101+safiirahsfh@users.noreply.github.com>
2023-06-26 16:34:01 +0700

    update

--- UAS-PBO/src/main/java/Bakery/UASPBO.java
+++ UAS-PBO/src/main/java/Bakery/UASPBO.java
@@ -11,12 +11,12 @@ import java.util.Scanner;
  * @author bangu
  */
 public class UASPBO {
-    private int manisVarian1;
-    private int manisVarian2;
-    private int manisVarian3;
-    private int tawarVarian1;
-    private int tawarVarian2;
-    private int pizzaVarian1;
+    private int ordManisVarian1;
+    private int ordManisVarian2;
+    private int ordManisVarian3;
+    private int ordTawarVarian1;
+    private int ordTawarVarian2;
+    private int ordPizzaVarian1;
     
     private Scanner scanner = new Scanner(System.in);
     private ManisVar1 manisVar1 = new ManisVar1();
@@ -25,39 +25,51 @@ public class UASPBO {
     private TawarVar1 tawarVar1 = new TawarVar1();
     private TawarVar2 tawarVar2 = new TawarVar2();
     private PizzaVar1 pizzaVar1 = new PizzaVar1();
+    private ResepManis resepManis = new ResepManis();
+    private ResepTawar resepTawar = new ResepTawar();
+    private ResepPizza resepPizza = new ResepPizza();
     
     
     public void input(){
         System.out.println("**********\nORDERAN\n**********");
         System.out.println("Roti manis  : ");
         System.out.print("    a. Varian 1 : ");
-        manisVarian1 = scanner.nextInt();
+        ordManisVarian1 = scanner.nextInt();
         System.out.print("    b. Varian 2 : ");
-        manisVarian2 = scanner.nextInt();
+        ordManisVarian2 = scanner.nextInt();
         System.out.print("    c. Varian 3 : ");
-        manisVarian3 = scanner.nextInt();
+        ordManisVarian3 = scanner.nextInt();
         System.out.println("Roti tawar  : ");
         System.out.print("    a. Varian 1 : ");
-        tawarVarian1 = scanner.nextInt();
+        ordTawarVarian1 = scanner.nextInt();
         System.out.print("    b. Varian 2 : ");
-        tawarVarian2 = scanner.nextInt();
+        ordTawarVarian2 = scanner.nextInt();
         System.out.println("Pizza       : ");
         System.out.print("    a. Varian 1 : ");
-        pizzaVarian1 = scanner.nextInt();
+        ordPizzaVarian1 = scanner.nextInt();
         
         output();
     }
     
     public void output(){
-        if (manisVarian1 > 0) {
-            System.out.println("Harga roti manis varian 1 : " + manisVar1.hargaVar1());
+        if (ordManisVarian1 > 0) {
+            manis();
+            System.out.println("Harga roti manis varian 1 per pcs : " + manisVar1.hargaVar1());
         }
-    }
-    
-    public void hitung(){
         
     }
-
+    
+    public void manis(){
+        System.out.println("Bahan yang diperlukan : ");
+            System.out.println("    1. Tepung Terigu    : " + resepManis.adonanTepungTerigu() * ordManisVarian1 + "gram");
+            System.out.println("    2. Gula Pasir       : " + resepManis.adonanGulaPasir() * ordManisVarian1 + "gram");
+            System.out.println("    3. Butter           : " + resepManis.adonanButter() * ordManisVarian1 + "gram");
+            System.out.println("    4. Ragi             : " + resepManis.adonanRagi() * ordManisVarian1 + "gram");
+            System.out.println("    5. Susu Bubuk       : " + resepManis.adonanSusuBubuk() * ordManisVarian1 + "gram");
+            System.out.println("    6. Susu Cair        : " + resepManis.adonanSusuCair() * ordManisVarian1 + "gram");
+            System.out.println("    7. Telur            : " + resepManis.adonanTelur() * ordManisVarian1 + "gram");
+            System.out.println("    8. Es Batu          : " + resepManis.adonanEsBatu() * ordManisVarian1 + "gram");
+    }
     public static void main(String[] args) {
         UASPBO order = new UASPBO();
         order.input();
